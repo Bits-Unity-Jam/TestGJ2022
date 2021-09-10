62e00d1b0774436fa8421f6b9afa5831cfa9ed47
Wadzhamah <semenbindu@gmail.com>
2021-09-10 11:03:01 +0300

    .
    .

--- Runner/Assets/Scripts/Mechanics/Paths/Segment.cs
+++ Runner/Assets/Scripts/Mechanics/Paths/Segment.cs
@@ -1,16 +1,37 @@
+using Mechanics.Obstacles;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Management;
 
 namespace Mechanics.Paths
 {
     public class Segment : MonoBehaviour
     {
+        [SerializeField] private List<ObstacleSpawnPoint> obstaclesSpawnPoints;
+        //private List<Obstacle> spawnedObstacles;
+        
+       // public List<ObstaclesSpawnPoint> ObstaclesSpawnPoints { get => obstaclesSpawnPoints; }
+        
+
         [SerializeField]
         private Transform frontPoint, backPoint;
 
         public Transform BorderFront { get => frontPoint; }
         public Transform BorderBack { get =>  backPoint; }
+
+        private List<Obstacle> spawnedObstacles = new List<Obstacle>();
+        private void OnEnable()
+        {
+
+            Spawn();
+
+
+        }
+        void Spawn()
+        {
+            spawnedObstacles.Add(Management.ObstacleSpawner.Instance.GetObstacle());
+        }
 
     }
 }
